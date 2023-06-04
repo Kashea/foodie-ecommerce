390cc89b26e0fb9afdce183df7f497fd2fba69a5
Kashea <camrynchelsea01@gmail.com>
2023-06-05 07:15:42 +0800

    Cart Page Creation-01

--- utils/data.js
+++ utils/data.js
@@ -1,40 +1,40 @@
 const data = {
     products: [
         {
-            name: 'Sinigang na Baboy',
-            slug: 'Sinigang na Baboy',
-            category: 'Filipino',
-            image: '/images/product_9.01.jpg',
-            price: 300,
-            brand: 'Gilligans',
+            name: 'Pork Katsudon',
+            slug: 'Pork Katsudon',
+            category: 'Japanese',
+            image: '/images/product_13.1.jpg',
+            price: 325,
+            brand: 'TokyoTokyo',
             rating: 4.5,
             numReviews: 8,
             countInStock: 20,
-            description: 'A sour tamarind soup base'
+            description: 'Japanese pork cutlet rice bowl made with Tonkatsu, eggs, and sauteed onions simmered in a sweet and savory sauce.'
         },
         {
-            name: 'Sinigang na Baboy',
-            slug: 'Sinigang na Baboy',
+            name: 'Sinigang na Bangus',
+            slug: 'Sinigang na Bangus',
             category: 'Filipino',
             image: '/images/product_9.01.jpg',
-            price: 300,
+            price: 380,
             brand: 'Gilligans',
             rating: 4.5,
             numReviews: 8,
             countInStock: 20,
-            description: 'A sour tamarind soup base'
+            description: 'A tamarind-based soup that is known for its sour and savory taste.'
         },
         {
-            name: 'Sinigang na Baboy',
-            slug: 'Sinigang na Baboy',
-            category: 'Filipino',
-            image: '/images/product_9.01.jpg',
-            price: 300,
-            brand: 'Gilligans',
+            name: 'Chicken Cacciatore',
+            slug: 'Chicken Cacciatore',
+            category: 'Italian',
+            image: '/images/product_31.1.png',
+            price: 720,
+            brand: 'Italiannis',
             rating: 4.5,
             numReviews: 8,
             countInStock: 20,
-            description: 'A sour tamarind soup base'
+            description: 'Roasted chicken, onions, bellpeppers, mushrooms simmered in rich red wine tomato '
         },
         {
             name: 'Sinigang na Baboy',
