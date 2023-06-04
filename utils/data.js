62a208cf6b95d56c94767a07e1359497655513fb
Kashea <camrynchelsea01@gmail.com>
2023-06-04 22:42:30 +0800

    add to cart functionality added

---
+++ utils/data.js
@@ -0,0 +1,90 @@
+const data = {
+    products: [
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        },
+        {
+            name: 'Sinigang na Baboy',
+            slug: 'Sinigang na Baboy',
+            category: 'Filipino',
+            image: '/images/product_9.01.jpg',
+            price: 300,
+            brand: 'Gilligans',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A sour tamarind soup base'
+        }
+    ]
+}
+
+export default data;
